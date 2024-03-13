d4bb38d67ff26b2473cc99e3b6ade7a15076a251
Aditya Ratna Shakya <aditya.shakya@r3.com>
2024-03-13 21:24:20 +0000

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 docker rmi -f sdb-ui:latest
 
-docker build --no-cache -t sdb-ui:latest .
+docker build -t sdb-ui:latest .
 
 kubectl apply -f k8s/deployment.yaml -n sdb
 
-kubectl delete pods --all -n sdb
+kubectl delete pod -n sdb -l app=frontend
